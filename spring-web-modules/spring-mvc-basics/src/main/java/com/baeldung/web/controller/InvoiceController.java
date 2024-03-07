bfd430f909c475caa95546af12a0019bf20afb49
Fahrek <andresgqjob@gmail.com>
2024-03-07 19:33:27 +0100

    Creación del controlador InvoiceController

---
+++ spring-web-modules/spring-mvc-basics/src/main/java/com/baeldung/web/controller/InvoiceController.java
@@ -0,0 +1,25 @@
+package com.baeldung.web.controller;
+
+import com.baeldung.model.Invoice;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/invoice")
+public class InvoiceController {
+
+    @GetMapping
+    public String showForm(Model model) {
+        model.addAttribute("invoice", new Invoice());
+        return "invoice/create";
+    }
+
+    @PostMapping
+    public String submitForm(@ModelAttribute("invoice") Invoice invoice) {
+        return "invoice/summary";
+    }
+}
